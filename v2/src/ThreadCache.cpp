7ff284b228b833f9b0294d68b0ccf35e43a281a8
sean <wanjx0701@gmail.com>
2026-08-05 16:55:24 +0800

    feat(allocate/deallocate): ThreadCache类的方法实现

---
+++ v2/src/ThreadCache.cpp
@@ -0,0 +1,138 @@
+# include "../include/ThreadCache.h"
+# include "../include/CentralCache.h"
+
+
+namespace SeanMemoryPool
+{
+
+// allocate 的实现
+void* ThreadCache::allocate(size_t size)
+{
+    if (size == 0)
+    {
+        size = ALIGNMENT;   // 至少分配一个对齐大小
+    }
+    if (size > MAX_BYTES)
+    {
+        return malloc(size);    // 大对象直接从系统分配
+    }
+    
+    // 获取对应大小空闲链表的索引
+    size_t index = SizeClass::getIndex(size);   
+
+    // 检查线程本地自由链表
+    // 如果 freeList_[index] 不为空，表示该链表中有可用内存块
+    if (void* ptr = freeList_[index])
+    {
+        // 将freeList_[index]指向的内存块的下一个内存块地址（取决于内存块的实现）
+        freeList_[index] = *reinterpret_cast<void**>(ptr);
+        // 更新对应链表的长度
+        freeListSize_[index]--;
+        return ptr;
+    }
+
+    // 如果线程本地自由链表为空，则从中心缓存获取一批内存
+    return fetchFromCentralCache(index);
+}
+
+// 内存释放
+void ThreadCache::deallocate(void* ptr, size_t size)
+{
+    if (size > MAX_BYTES)
+    {
+        free(ptr);
+        return;
+    }
+
+    // 获取对应大小空闲链表的索引
+    size_t index = SizeClass::getIndex(size);
+
+    // 插入到线程本地空闲连表,并更新链表长度
+    *reinterpret_cast<void**>(ptr) = freeList_[index];
+    freeList_[index] = ptr;
+    freeListSize_[index]++;
+
+    // 判断是否需要将部分内存回收给中心缓存
+    if (shouldReturnToCentralCache(index))
+    {
+        returnToCentralCache(freeList_[index], size);
+    }
+}
+
+// 从中心缓存获取内存
+void* ThreadCache::fetchFromCentralCache(size_t index)
+{
+    // 从中心缓存批量获取内存
+    void* start = CentralCache::getInstance().fetchRange(index);
+    if (!start)
+        return;
+
+    // 取回一个, 其余放入空闲链表
+    void* result = start;
+    freeList_[index] = *reinterpret_cast<void**>(start);
+
+    // 更新空闲链表的大小
+    void* cur = freeList_[index];
+    while (cur != nullptr)
+    {
+        freeListSize_[index]++;
+        cur = *reinterpret_cast<void**>(cur); // 遍历下一个内存块
+    }
+
+    return result;
+}
+
+// 将内存归还给中心缓存
+void ThreadCache::returnToCentralCache(void* start, size_t size)
+{
+    // 获取对应大小空闲链表的索引
+    size_t index = SizeClass::getIndex(size);
+
+    // 获取对齐后的实际块大小
+    size_t alignedSize = SizeClass::roundUp(size);
+
+    // 计算要归还的内存块数量
+    size_t batchNum = freeListSize_[index];
+    if (batchNum <= 1) return;  // 如果只有一个块, 则不归还
+
+    // 保留一部分在ThreadCache中(比如保留1/4)
+    size_t keepNum = std::max(batchNum / 4, size_t(1));
+    size_t returnNum = batchNum - keepNum;
+
+    // 将内存块串成链表
+    char* cur = static_cast<char*>(start);
+    // 使用对齐后的大小计算分割点
+    char* split_node = cur;
+    for (size_t i = 0; i < keepNum - 1; ++i)
+    {
+        split_node = reinterpret_cast<char*>(*reinterpret_cast<void**>(split_node));
+        if (split_node == nullptr)
+        {
+            // 如果链表提前结束, 更新实际的返回数量
+            returnNum = batchNum - (i + 1);
+            break;
+        }
+    }
+
+    if (split_node != nullptr)
+    {
+        // 将要返回的部分和要保留的部分断开
+        void* nextNode = *reinterpret_cast<void**>(split_node);
+        *reinterpret_cast<void**>(split_node) = nullptr;
+
+        // 更新ThreadCache的空闲链表
+        freeList_[index] = start;
+
+        // 更新自由链表的大小
+        freeListSize_[index] = keepNum;
+
+        // 将剩余部分返还给CentralCache
+        if (returnNum > 0 && nextNode != nullptr)
+        {
+            CentralCache::getInstance().returnRange(nextNode, returnNum * alignedSize, index);
+        }
+    }
+
+}
+
+}
