0eef6326d26b0296d2c73e6ff98970b174aa0019
sean <wanjx0701@gmail.com>
2026-08-12 17:34:10 +0800

    feat: MemoryPool 声明

---
+++ v2/include/MemoryPool.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "ThreadCache.h"
+
+
+namespace SeanMemoryPool
+{
+
+class MemoryPool
+{
+public:
+    static void* allocate(size_t size)
+    {
+        return ThreadCache::getInstance()->allocate(size);
+    }
+
+    static void deallocate(void* ptr, size_t size)
+    {
+        ThreadCache::getInstance()->deallocate(ptr, size);
+    }
+};
+
+}
